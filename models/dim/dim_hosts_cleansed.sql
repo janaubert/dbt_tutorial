8e777925f91685e601bd6a79c9920c359b20adde
Jan Aubert <janaubert92@gmail.com>
2023-03-11 15:15:01 +0100

    first commit

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,12 @@
+WITH src_hosts AS (
+    SELECT * FROM {{ ref('src_hosts')}}
+    )
+
+SELECT 	
+    host_id,
+    CASE WHEN host_name IS NULL THEN 'Anonymous'
+    ELSE host_name END AS host_name,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM src_hosts
---
+++ models/src/src_reviews.sql
@@ -0,0 +1,13 @@
+WITH raw_reviews AS (
+SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
+)
+
+SELECT 
+	listing_id,
+	date as review_date,
+    reviewer_name as name,
+    comments as review_text,
+    sentiment as review_sentiment
+
+FROM
+	raw_reviews
